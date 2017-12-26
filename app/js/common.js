df62ccd751d7f420f504374a1f6b17950156afcf
Evgen Skaskevich <skaskevich23@gmail.com>
2017-12-27 01:59:54 +0300

    catalog1-1

--- app/js/common.js
+++ app/js/common.js
@@ -85,3 +85,20 @@ $(".mobile-mnu").click(function() {
   return false;
 
 });
+
+$(document).ready(function(){
+ 
+$(window).scroll(function(){
+if ($(this).scrollTop() > 100) {
+$('.scrollup').fadeIn();
+} else {
+$('.scrollup').fadeOut();
+}
+});
+ 
+$('.scrollup').click(function(){
+$("html, body").animate({ scrollTop: 0 }, 600);
+return false;
+});
+ 
+});
